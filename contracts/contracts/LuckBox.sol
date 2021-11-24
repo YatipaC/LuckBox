f4eaaa5937bd505b49aa2066307165058dd2c756
pisuthd <pisuth.dae@gmail.com>
2021-11-24 18:32:45 +0700

    refactor: receiving ERC-721 NFT, deduct fees /w ETH, randomized per each slot + test

--- contracts/contracts/LuckBox.sol
+++ contracts/contracts/LuckBox.sol
@@ -4,16 +4,27 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol"; 
+import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/introspection/ERC165.sol";
+// import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+// import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+// import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 import "./interfaces/IUniswapV2Router02.sol";
 import "./interfaces/ILuckbox.sol";
+// import "./utility/LibMath.sol";
 
-contract LuckBox is VRFConsumerBase, Ownable, ReentrancyGuard, ILuckbox {
+contract LuckBox is VRFConsumerBase, Ownable, ReentrancyGuard, ILuckbox, IERC721Receiver, ERC165, ERC721Holder {
     using SafeMathChainlink for uint256;
     using SafeERC20 for IERC20;
+    // using LibMathSigned for int256;
+    // using LibMathUnsigned for SafeMathChainlink;
 
     mapping(bytes32 => address) private requestIdToAddress;
 
@@ -23,154 +34,288 @@ contract LuckBox is VRFConsumerBase, Ownable, ReentrancyGuard, ILuckbox {
 
     bool public locked = false;
 
+    uint256 public override ticketPrice;
+
+    // Chainlink
     address public constant VRF_COORDINATOR =
         0x3d2341ADb2D31f1c5530cDC622016af293177AE0;
     address public constant LINK_TOKEN =
         0xb0897686c545045aFc77CF20eC7A532E3120E0F1;
     bytes32 public constant KEY_HASH =
         0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da;
-    uint256 public constant FEE = 0.0001 * 10**18;
+    uint256 public constant FEE = 100000000000000; // 0.0001 LINK
+
+    // Quickswap
+
     IUniswapV2Router02 public constant ROUTER =
         IUniswapV2Router02(0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff);
-    address public constant TAMG_TOKEN =
-        0x53BDA082677a4965C79086D3Fe69A6182d6Af1B8;
-    address public constant USDC_TOKEN =
-        0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-    address[] public TAMG_TO_LINK_PATH = [TAMG_TOKEN, USDC_TOKEN, LINK_TOKEN];
-
-    uint256 public override ticketPrice;
-
-    struct Box {
-        IERC721 factory;
+    // address public constant USDC_TOKEN =
+    //     0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    
+    struct Slot {
+        address assetAddress;
         uint256 tokenId;
-        uint256 winnerNumber;
-        bool isClaimed;
+        bool is1155;
+        bool locked;
+        uint256 randomnessChance;
+        bool pendingWinnerToClaim; 
+        address winner;
     }
-    Box[] public nftLists;
 
-    event SetTicketPrice(uint256 ticketPrice);
-    event GetLucky(address indexed randomer, bytes32 requestId);
+    mapping(uint8 => Slot) public list;
+
+    struct Result {
+        bytes32 requestId;
+        address drawer;
+        bool won;
+        uint8 slot;
+        uint256 output;
+        uint256 eligibleRange;
+    }
+
+    mapping(uint256 => Result) public result;
+
+    uint256 public resultCount;
+
+    uint8 constant public MAX_SLOT = 9;
+
+    event UpdatedTicketPrice(uint256 ticketPrice);
+    event Draw(address indexed drawer, bytes32 requestId);
     event ClaimNft(address indexed receiver, address factory, address tokenId);
-    event AddNft(address factory, uint256 tokenId);
-    event RemoveNft(address factory, uint256 tokenId);
+    event DepositedNft(uint8 slotId, address assetAddress, uint256 tokenId, bool is1155, uint256 randomness);
+    event WithdrawnNft(uint8 slotId);
+    event Drawn(address indexed drawer, bool won );
+    event Claimed(uint8 slotId, address winner);
 
     constructor(
         string memory _name,
         string memory _symbol,
-        // IERC721[] memory _factory,
-        // uint256[] memory _tokenId,
         uint256 _ticketPrice
     ) public VRFConsumerBase(VRF_COORDINATOR, LINK_TOKEN) {
+        require( _ticketPrice != 0 , "Invalid ticket price");
+
         name = _name;
         symbol = _symbol;
         ticketPrice = _ticketPrice;
 
-        // for (uint256 i = 0; i < _factory.length; i++) {
-        //   IERC721(_factory[i]).safeTransferFrom(
-        //     msg.sender,
-        //     address(this),
-        //     _tokenId[i]
-        //   );
+        _registerInterface(IERC721Receiver.onERC721Received.selector);
 
-        //   nftLists.push(
-        //     Box({
-        //       factory: _factory[i],
-        //       tokenId: _tokenId[i],
-        //       winnerNumber: i,
-        //       isClaimed: false
-        //     })
-        //   );
-        // }
     }
 
-    function setTicketPrice(uint256 _ticketPrice) public onlyOwner {
-        ticketPrice = _ticketPrice;
-        emit SetTicketPrice(_ticketPrice);
-    }
+    function draw() public nonReentrant payable returns (bytes32 requestId) {
+        require(msg.value == ticketPrice , "Payment is not attached");
 
-    function addNft(IERC721 _factory, uint256 _tokenId) public onlyOwner {
-        IERC721(_factory).safeTransferFrom(msg.sender, address(this), _tokenId);
-        nftLists.push(
-            Box({
-                factory: _factory,
-                tokenId: _tokenId,
-                winnerNumber: nftLists.length,
-                isClaimed: false
-            })
-        );
-
-        emit AddNft(address(_factory), _tokenId);
-    }
-
-    function removeNft(uint256 _nftId) public onlyOwner nonReentrant {
-        Box storage nft = nftLists[_nftId];
-        require(address(nft.factory) != address(0), "No nft!");
-        require(!nft.isClaimed, "Claimed already.");
-        IERC721 factory = nft.factory;
-        uint256 tokenId = nft.tokenId;
-        nft.factory = IERC721(address(0));
-        nft.tokenId = 0;
-        nft.winnerNumber = 0;
-        nft.isClaimed = false;
-        IERC721(factory).transferFrom(address(this), msg.sender, tokenId);
-        emit RemoveNft(address(factory), tokenId);
-    }
-
-    function getLucky() public nonReentrant returns (bytes32 requestId) {
-        IERC20(TAMG_TOKEN).safeTransferFrom(
-            msg.sender,
-            address(this),
-            ticketPrice
-        );
-        uint256[] memory amount = ROUTER.getAmountsIn(FEE, TAMG_TO_LINK_PATH);
-        uint256 tamgToLinkAmount = amount[amount.length.sub(1)];
-        require(tamgToLinkAmount >= ticketPrice, "Not enoung TAMG.");
-        ROUTER.swapTokensForExactTokens(
-            FEE,
-            tamgToLinkAmount,
-            TAMG_TO_LINK_PATH,
-            address(this),
-            block.timestamp
-        );
         require(
             IERC20(LINK_TOKEN).balanceOf(address(this)) >= FEE,
-            "Not enoung link."
+            "Insufficient LINK to proceed VRF"
         );
+
         requestId = requestRandomness(KEY_HASH, FEE);
         requestIdToAddress[requestId] = msg.sender;
 
-        emit GetLucky(msg.sender, requestId);
+        emit Draw(msg.sender, requestId);
+    }
+
+    function getLucky() public nonReentrant returns (bytes32 requestId) {
+        // IERC20(TAMG_TOKEN).safeTransferFrom(
+        //     msg.sender,
+        //     address(this),
+        //     ticketPrice
+        // );
+        // uint256[] memory amount = ROUTER.getAmountsIn(FEE, TAMG_TO_LINK_PATH);
+        // uint256 tamgToLinkAmount = amount[amount.length.sub(1)];
+        // require(tamgToLinkAmount >= ticketPrice, "Not enoung TAMG.");
+        // ROUTER.swapTokensForExactTokens(
+        //     FEE,
+        //     tamgToLinkAmount,
+        //     TAMG_TO_LINK_PATH,
+        //     address(this),
+        //     block.timestamp
+        // );
+        // require(
+        //     IERC20(LINK_TOKEN).balanceOf(address(this)) >= FEE,
+        //     "Not enoung link."
+        // );
+        // requestId = requestRandomness(KEY_HASH, FEE);
+        // requestIdToAddress[requestId] = msg.sender;
+
+        // emit GetLucky(msg.sender, requestId);
+    }
+
+    // find the current winning rates
+    function winningRates() public view returns (uint256) {
+        uint256 increment = 0;
+        for (uint8 i = 0; i < MAX_SLOT; i++) {
+
+            Slot storage slot = list[i];
+
+            if (slot.locked && slot.pendingWinnerToClaim == false) {
+                increment += slot.randomnessChance;
+            }
+        }
+        return increment;
+    }
+
+    function parseRandomUInt256(uint256 input) public pure returns (uint256) {
+        return _parseRandomUInt256(input);
+    }
+
+    function totalEth() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function claimNft(uint8 _slotId) public {
+        require( MAX_SLOT > _slotId, "Invalid slot ID" );
+        require( list[_slotId].locked == true , "The slot is empty" );
+        require( list[_slotId].pendingWinnerToClaim == true, "Still has no winner");
+        require( list[_slotId].winner == msg.sender, "The caller is not the winner");
+
+        if (list[_slotId].is1155) {
+            // IERC1155(list[_slotId].assetAddress).safeTransferFrom(  address(this), msg.sender , list[_slotId].tokenId , 1 , "0x00");
+        } else {
+            IERC721(list[_slotId].assetAddress).safeTransferFrom(  address(this), msg.sender,  list[_slotId].tokenId );
+        }
+
+        list[_slotId].locked = false;
+        list[_slotId].assetAddress = address(0);
+        list[_slotId].tokenId = 0;
+        list[_slotId].is1155 = false;
+        list[_slotId].randomnessChance = 0;
+        list[_slotId].pendingWinnerToClaim = false;
+
+        emit Claimed(_slotId, msg.sender);
+    }
+
+    // ONLY OWNER CAN PROCEED
+
+    // for local testing 
+    // _randomNumber must be in between 0 - 2^10**256
+    function forceDraw( uint256 _randomNumber) public payable onlyOwner nonReentrant {
+        require(msg.value == ticketPrice , "Payment is not attached");
+        
+        _draw(_randomNumber, msg.sender , "0x00");
+    } 
+
+    function withdrawAllEth() public onlyOwner nonReentrant {
+        uint amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Failed to send Ether");
+    }
+
+    function setTicketPrice(uint256 _ticketPrice) public onlyOwner nonReentrant {
+        require( _ticketPrice != 0 , "Invalid ticket price");
+
+        ticketPrice = _ticketPrice;
+        emit UpdatedTicketPrice(_ticketPrice);
     }
 
     function setLock(bool _locked) public onlyOwner nonReentrant {
         locked = _locked;
     }
 
+    // not supported ERC-1155 for now
+    // randomness value -> 1% = 100, 10% = 1000 and not allows more than 10% per each slot
+    function depositNft(uint8 _slotId , uint256 _randomness, address _assetAddress, uint256 _tokenId, bool _is1155) public nonReentrant onlyOwner isReady {
+        require( MAX_SLOT > _slotId, "Invalid slot ID" );
+        require( 1000 >= _randomness , "Randomness value must be between 0-1000");
+        require( _is1155 == false , "Not supported ERC-1155 yet");
+
+        // take the NFT
+        if (_is1155) {
+            // IERC1155(_assetAddress).safeTransferFrom( msg.sender , address(this), _tokenId , 1 , "0x00");
+        } else {
+            IERC721(_assetAddress).safeTransferFrom( msg.sender, address(this), _tokenId );
+        }
+
+        list[_slotId].locked = true;
+        list[_slotId].assetAddress = _assetAddress;
+        list[_slotId].tokenId = _tokenId;
+        list[_slotId].is1155 = _is1155;
+        list[_slotId].randomnessChance = _randomness;
+        list[_slotId].pendingWinnerToClaim = false;
+    
+        emit DepositedNft(_slotId, _assetAddress, _tokenId, _is1155, _randomness);
+    }
+
+    function withdrawNft(uint8 _slotId)  public nonReentrant onlyOwner isReady {
+        require( MAX_SLOT > _slotId, "Invalid slot ID" );
+        require( list[_slotId].locked == true , "The slot is empty" );
+        require( list[_slotId].pendingWinnerToClaim == false, "The asset locked is being claimed by the winner");
+
+        if (list[_slotId].is1155) {
+            // IERC1155(list[_slotId].assetAddress).safeTransferFrom(  address(this), msg.sender , list[_slotId].tokenId , 1 , "0x00");
+        } else {
+            IERC721(list[_slotId].assetAddress).safeTransferFrom(  address(this), msg.sender,  list[_slotId].tokenId );
+        }
+
+        list[_slotId].locked = false;
+        list[_slotId].assetAddress = address(0);
+        list[_slotId].tokenId = 0;
+        list[_slotId].is1155 = false;
+        list[_slotId].randomnessChance = 0;
+        list[_slotId].pendingWinnerToClaim = false;
+
+        emit WithdrawnNft(_slotId);
+    }
+
     // PRIVATE FUNCTIONS
 
+    // callback from Chainlink VRF
     function fulfillRandomness(bytes32 requestId, uint256 _randomness)
         internal
         override
     {
-        uint256 randomNubmer = _randomness.mod(100).add(1);
         address receiver = requestIdToAddress[requestId];
-        for (uint256 i = 0; i < nftLists.length; i++) {
-            Box storage nft = nftLists[i];
-            if (nft.winnerNumber == randomNubmer && !nft.isClaimed) {
-                nft.isClaimed = true;
-                _claimNft(i, receiver);
-            }
-        }
+        _draw(_randomness, receiver , requestId);
     }
 
-    function _claimNft(uint256 _nftId, address _receiver) internal {
-        Box storage nft = nftLists[_nftId];
-        IERC721(nft.factory).safeTransferFrom(
-            address(this),
-            _receiver,
-            nft.tokenId
-        );
+    function _parseRandomUInt256(uint256 input) internal pure returns (uint256) {
+        return input.mod(10000);
     }
+
+    function _draw(uint256 _randomNumber, address _drawer, bytes32 _requestId) internal {
+        uint256 parsed = _parseRandomUInt256(_randomNumber); // parse into 0-10000 or 0.00-100.00%
+        uint256 increment = 0;
+        bool won = false;
+        uint8 winningSlot = 0;
+
+        for (uint8 i = 0; i < MAX_SLOT; i++) {
+            Slot storage slot = list[i];
+            if (slot.locked && slot.pendingWinnerToClaim == false) {
+                increment += slot.randomnessChance;
+                // keep incrementing until the random number has been hitted
+                if (increment > parsed && !won) {
+                    slot.pendingWinnerToClaim = true;
+                    slot.winner = _drawer;
+                    
+                    won = true;
+                    winningSlot = i;
+                }
+            }
+        }
+
+        // keep track of the result
+        result[resultCount].requestId = _requestId;
+        result[resultCount].drawer = _drawer;
+        result[resultCount].won = won;
+        result[resultCount].slot = winningSlot;
+        result[resultCount].output = parsed;
+        result[resultCount].eligibleRange = increment;
+
+        resultCount += 1;
+
+        emit Drawn(_drawer, won);
+    }
+
+    // function _claimNft(uint256 _nftId, address _receiver) internal {
+    //     Box storage nft = nftLists[_nftId];
+    //     IERC721(nft.factory).safeTransferFrom(
+    //         address(this),
+    //         _receiver,
+    //         nft.tokenId
+    //     );
+    // }
+
 
     modifier isReady() {
         require(
@@ -178,6 +323,6 @@ contract LuckBox is VRFConsumerBase, Ownable, ReentrancyGuard, ILuckbox {
             "The contract is locked by the owner"
         );
         _;
-    }
+    }    
 
 }
