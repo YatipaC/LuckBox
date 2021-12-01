07c7c8fcce90c0c9052336024282700f945c0766
Kantapat Chankasem <kantapatchankasem@Kantapat.local>
2021-12-01 12:31:47 +0700

    add support erc1155 and test case

--- contracts/test/LuckBox.js
+++ contracts/test/LuckBox.js
@@ -2,8 +2,9 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 const { fromEther, toEther } = require("./Helpers")
 
-
 let luckBox
+let luckBox1155
+
 let erc721
 let erc1155
 
@@ -12,12 +13,11 @@ let alice
 let bob
 
 describe("LuckBox", () => {
-
   before(async () => {
+    ;[admin, alice, bob] = await ethers.getSigners()
 
-    [admin, alice, bob] = await ethers.getSigners();
-
-    const LuckBox = await ethers.getContractFactory("LuckBox");
+    const LuckBox = await ethers.getContractFactory("LuckBox")
+    const LuckBox1155 = await ethers.getContractFactory("LuckBox")
 
     luckBox = await LuckBox.deploy(
       "My Luckbox",
@@ -25,26 +25,30 @@ describe("LuckBox", () => {
       toEther(0.1) // 0.1 MATIC
     )
 
-    const MockERC721 = await ethers.getContractFactory("MockERC721");
-    const MockERC1155 = await ethers.getContractFactory("MockERC1155");
+    luckBox1155 = await LuckBox1155.deploy(
+      "My Luckbox",
+      "LUCKBOX",
+      toEther(0.1) // 0.1 MATIC
+    )
+
+    const MockERC721 = await ethers.getContractFactory("MockERC721")
+    const MockERC1155 = await ethers.getContractFactory("MockERC1155")
 
     erc721 = await MockERC721.deploy("Mock NFT", "MOCK")
-    erc1155 = await MockERC1155.deploy("https://api.cryptokitties.co/kitties/{id}")
-
+    erc1155 = await MockERC1155.deploy(
+      "https://api.cryptokitties.co/kitties/{id}"
+    )
   })
 
   it("Checks initial params", async () => {
-
     expect(await luckBox.name()).to.equal("My Luckbox")
     expect(await luckBox.symbol()).to.equal("LUCKBOX")
     expect(await luckBox.ticketPrice()).to.equal(toEther(0.1))
 
     expect(await luckBox.owner()).to.equal(admin.address)
-
   })
 
   it("Deposit & Withdraw ERC-721 NFTs to all slots", async () => {
-
     const tokenIds = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
     // minting ERC-721
@@ -57,7 +61,7 @@ describe("LuckBox", () => {
     // depositing
     for (let id of tokenIds) {
       // await erc721.approve(luckBox.address, id)
-      await luckBox.depositNft(id, (1 * 100), erc721.address, id, false)
+      await luckBox.depositNft(id, 1 * 100, erc721.address, id, false)
     }
 
     expect(await erc721.balanceOf(admin.address)).to.equal(0)
@@ -81,9 +85,44 @@ describe("LuckBox", () => {
   })
 
   it("Deposit & Withdraw ERC-1155 NFTs to all slots", async () => {
-
     // FIXME : SUPPORT ERC-1155
+    const tokenIds = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
+    // minting ERC-1155
+    for (let id of tokenIds) {
+      await erc1155.mint(admin.address, id, 1, "0x00")
+    }
+
+    await erc1155.setApprovalForAll(luckBox1155.address, true)
+
+    // depositing
+    for (let id of tokenIds) {
+      // await erc721.approve(luckBox.address, id)
+      await luckBox1155.depositNft(id, 1 * 100, erc1155.address, id, true)
+    }
+
+    for (let id of tokenIds) {
+      expect(await erc1155.balanceOf(admin.address, id)).to.equal(0)
+    }
+
+    // verifying
+    for (let id of tokenIds) {
+      const slotData = await luckBox1155.list(id)
+      expect(slotData[0]).to.equal(erc1155.address)
+      expect(slotData[1]).to.equal(id)
+      expect(slotData[2]).to.equal(true)
+      expect(slotData[3]).to.equal(true)
+      expect(slotData[4]).to.equal(100)
+    }
+
+    // withdrawing
+    for (let id of tokenIds) {
+      await luckBox1155.withdrawNft(id)
+    }
+
+    for (let id of tokenIds) {
+      expect(await erc1155.balanceOf(admin.address, id)).to.equal(1)
+    }
   })
 
   it("Force drawing", async () => {
@@ -92,7 +131,7 @@ describe("LuckBox", () => {
 
     for (let id of tokenIds) {
       // use 10% winning chance
-      await luckBox.depositNft(id, (10 * 100), erc721.address, id, false)
+      await luckBox.depositNft(id, 10 * 100, erc721.address, id, false)
     }
 
     // winning rates should be at 90% ( 9 NFTs x 10% each)
@@ -107,11 +146,13 @@ describe("LuckBox", () => {
       "56677808388515418397845035688777921524411635120334930246921438203618707722478", // 2478
       "24789687723422206504093975672595709196520120609406715522373668926008795395244", // 5244
       "7892590353424658403775693550756982572540459138139681914848281068137357260205", // 205
-      "97305658150961137327832525889841521151674547403341010967027383752471741837607" // 7607
+      "97305658150961137327832525889841521151674547403341010967027383752471741837607", // 7607
     ]
 
     for (let random of randoms) {
-      await luckBox.forceDraw(ethers.BigNumber.from(random), { value: toEther(0.1) })
+      await luckBox.forceDraw(ethers.BigNumber.from(random), {
+        value: toEther(0.1),
+      })
     }
 
     // should able to claim 5 NFTs
@@ -124,17 +165,15 @@ describe("LuckBox", () => {
 
         // then claim it
         await luckBox.claimNft(id)
-
       } else {
         expect(slotData[5]).to.equal(false)
       }
-
     }
 
     expect(await erc721.balanceOf(admin.address)).to.equal(5)
 
     // winning rates should be reduced to 40%
-    expect((await luckBox.winningRates())).to.equal(4000)
+    expect(await luckBox.winningRates()).to.equal(4000)
 
     // check earning
     expect(await luckBox.totalEth()).to.equal(toEther(0.8))
@@ -153,7 +192,6 @@ describe("LuckBox", () => {
       if ([0, 2, 3, 4, 6].indexOf(i) !== -1) {
         expect(result[2]).to.true
       }
-
     }
 
     // verifying slot 0,2,3,4,6 are empty
@@ -166,9 +204,5 @@ describe("LuckBox", () => {
         expect(slotData[5]).to.equal(false)
       }
     }
-
-
   })
-
-
 })
